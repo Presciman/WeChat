ce8231ccb47a29f81a548dcc7e63869f3bf81e9b
Eric Sun <sunbaixi@163.com>
2019-11-13 15:57:14 -0800

    Add files via upload

---
+++ WeChatTest/Utility/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WeChatTest.DAL;
+
+namespace WeChatTest.Utility
+{
+    public static class Helper
+    {
+        static string connString = "Data Source=xuyuex.date,1433;Initial Catalog=StarrySkyDB;Persist Security Info=True;User ID=sa;Password=Nich0las";
+        
+    }
+}
